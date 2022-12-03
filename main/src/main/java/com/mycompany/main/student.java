5b3187531811491b89fe7cbc7327a71e9f5a30b1
Николай <mc_an@rambler.ru>
2022-12-03 10:52:05 +0700

    реализованны все арифметические методы,с учётом сокращения и флагов,безметода clonе

---
+++ main/src/main/java/com/mycompany/main/student.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author Администратор
+ */
+public class student {
+    
+    String name;
+   String secondname;
+   String lastname;
+    int age;
+    int salary;
+    student()
+    {
+       name="";
+   secondname="";
+   lastname="";
+   age=0;
+   salary=-1; 
+    }
+    student(String nmA,String nmB,String nmC,int ageP,int salaryP)
+    {
+    name= nmA;
+    secondname= nmB;
+    lastname= nmC;
+    age= ageP;
+     salary= salaryP;    
+    }
+    String ToFormatString()
+    {     
+    return"Student "+name+" "+secondname+" "+lastname+" "+age+" "+salary;
+}    
+    void SetSalary(int newsalary) // int []marka
+    {
+        salary = newsalary;
+    }
+}
