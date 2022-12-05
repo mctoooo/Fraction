2dfc0975c7e3fcc99e3ffb456d6ea13e83b06c2f
Николай <mc_an@rambler.ru>
2022-12-05 07:05:40 +0700

    +имплементация метода clonеable

--- main/src/main/java/com/mycompany/main/Fraction.java
+++ main/src/main/java/com/mycompany/main/Fraction.java
@@ -8,216 +8,215 @@ package com.mycompany.main;
  *
  * @author Администратор
  */
-public class Fraction {
-private int ch;
-private int zn;
-static private int countObj = 0;
-private static boolean flagCl=false;
-private boolean flagObj=false;
+public class Fraction implements Cloneable {
 
-Fraction()
-    {
-     ch = 0;
-     zn = 1;
-     countObj++;
+    private int ch;
+    private int zn;
+    static private int countObj = 0;
+    private static boolean flagCl = false;
+    private boolean flagObj = false;
+
+    Fraction() {
+        ch = 0;
+        zn = 1;
+        countObj++;
     }
 
-Fraction(int chA, int znA){
-countObj++;
-if(znA == 0)
-     {
-     ch = 0;
-     zn = 1;
-     }
-else
-{
-     int nod = Nod(chA,znA);
-     ch = flagCl==false?(chA):(chA/nod);
-     zn = flagCl==false?(znA):(znA/nod);
+    Fraction(int chA, int znA) {
+        countObj++;
+        if (znA == 0) {
+            ch = 0;
+            zn = 1;
+        } else {
+            int nod = Nod(chA, znA);
+            ch = flagCl == false ? (chA) : (chA / nod);
+            zn = flagCl == false ? (znA) : (znA / nod);
 
-}
-if(chA<0&&znA<0){
-     int nod = Nod(chA,znA);
-     ch = flagCl==false?(Math.abs(chA)):(Math.abs(chA)/nod);
-     zn = flagCl==false?(Math.abs(znA)):(Math.abs(znA)/nod);
-}
-if(chA>0&&znA<0){
-     int nod = Nod(chA,znA);
-     ch = flagCl==false?(-chA):(-(chA/nod));
-     zn = flagCl==false?(Math.abs(znA)):(Math.abs(znA)/nod);
-}
-}
-public static void setflagCl(boolean newFlagCl){
-     flagCl=newFlagCl;
-     }
+        }
+        if (chA < 0 && znA < 0) {
+            int nod = Nod(chA, znA);
+            ch = flagCl == false ? (Math.abs(chA)) : (Math.abs(chA) / nod);
+            zn = flagCl == false ? (Math.abs(znA)) : (Math.abs(znA) / nod);
+        }
+        if (chA > 0 && znA < 0) {
+            int nod = Nod(chA, znA);
+            ch = flagCl == false ? (-chA) : (-(chA / nod));
+            zn = flagCl == false ? (Math.abs(znA)) : (Math.abs(znA) / nod);
+        }
+    }
 
-void setflagObj(boolean newFlagObj){
-     flagObj=newFlagObj;
+    public static void setflagCl(boolean newFlagCl) {
+        flagCl = newFlagCl;
     }
-boolean getflagCl(){
-     return flagCl;
+
+    void setflagObj(boolean newFlagObj) {
+        flagObj = newFlagObj;
     }
-boolean getflagObj(){
-     return flagObj;
+
+    boolean getflagCl() {
+        return flagCl;
+    }
+
+    boolean getflagObj() {
+        return flagObj;
     }
 //метод сложения
-Fraction add(Fraction objF)
-     {
-     Fraction res = new Fraction (ch * objF.zn + objF.ch * zn,zn * objF.zn);
-         if(flagCl==true){
+
+    Fraction add(Fraction objF) {
+        Fraction res = new Fraction(ch * objF.zn + objF.ch * zn, zn * objF.zn);
+        if (flagCl == true) {
             res.reduse();
-            }
-     return res;
-
-     }
-// метод сравнения дробей
-boolean isEqual(Fraction objF)
-    {
-     if(objF == null)
-        return false;
-     if(ch == objF.ch && zn == objF.zn)
-        return true;
-     return false;
-     }
-String toFormatString()
-     {
-
-     return ch+"/"+zn;
-     }
-public static int getCountObject()
-     {
-     return countObj;
-     }
-
-void setZn(int znA){
-     this.zn = znA;
-     }
-void setCh(int chA){
-     ch = chA;
-     }
-
-int getZn(int znA){
-     return zn;
-     }
-int getCh(int chA){
-     return ch;
-     }
-
-void CopyFraction(Fraction obj)
-    {
-         if(obj!=null)
-         {
-             ch = obj.ch;
-             zn = obj.zn;
-         } 
-    else
-         {
-         ch = 0;
-         zn = 1;
         }
-}
+        return res;
+
+    }
+// метод сравнения дробей
+
+    boolean isEqual(Fraction objF) {
+        if (objF == null) {
+            return false;
+        }
+        if (ch == objF.ch && zn == objF.zn) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     *
+     * @return @throws CloneNotSupportedException
+     */
+    @Override
+    public Fraction clone() throws CloneNotSupportedException {
+        return (Fraction) super.clone();
+    }
+
+    String toFormatString() {
+
+        return ch + "/" + zn;
+    }
+
+    public static int getCountObject() {
+        return countObj;
+    }
+
+    void setZn(int znA) {
+        this.zn = znA;
+    }
+
+    void setCh(int chA) {
+        ch = chA;
+    }
+
+    int getZn(int znA) {
+        return zn;
+    }
+
+    int getCh(int chA) {
+        return ch;
+    }
+
+    void CopyFraction(Fraction obj) {
+        if (obj != null) {
+            ch = obj.ch;
+            zn = obj.zn;
+        } else {
+            ch = 0;
+            zn = 1;
+        }
+    }
 //метод деления
-Fraction dev(Fraction objF)throws Exception
-     {
-     if(objF==null || objF.ch==0)
-         throw new Exception("Devide error in Fraktion");
-     if(this.flagObj==true)
-    {
-        this.reduse();
+
+    Fraction dev(Fraction objF) throws Exception {
+        if (objF == null || objF.ch == 0) {
+            throw new Exception("Devide error in Fraktion");
+        }
+        if (this.flagObj == true) {
+            this.reduse();
+        }
+        if (objF.flagObj == true) {
+            objF.reduse();
+        }
+        Fraction res = new Fraction(ch * objF.zn, zn * objF.ch);
+        if (flagCl == true) {
+            res.reduse();
+        }
+        return res;
     }
-     if(objF.flagObj==true)
-     {
-         objF.reduse();
-     }
-         Fraction res = new Fraction (ch * objF.zn ,zn * objF.ch);
-     if(flagCl==true)
-         {
-         res.reduse();
-         }
-               return res;
-     }
 // метод вычитания
-Fraction subtraction(Fraction objF)
-     {
-     if(objF!=null|| objF.ch!=0){
-          if(this.flagObj==true)
-    {
-        this.reduse();
+
+    Fraction subtraction(Fraction objF) {
+        if (objF != null || objF.ch != 0) {
+            if (this.flagObj == true) {
+                this.reduse();
+            }
+            if (objF.flagObj == true) {
+                objF.reduse();
+            }
+            Fraction res = new Fraction(ch * objF.zn - objF.ch * zn, zn * objF.zn);
+            if (flagCl == true) {
+                res.reduse();
+            }
+            return res;
+        } else {
+            ch = 0;
+            zn = 1;
+            Fraction res = new Fraction(ch, zn);
+            return res;
+        }
     }
-     if(objF.flagObj==true)
-     {
-         objF.reduse();
-     }
-         Fraction res = new Fraction (ch * objF.zn - objF.ch * zn,zn * objF.zn);
-     if(flagCl==true)
-         {
-         res.reduse();
-         }
-     return res;
-     }
-     else{
-         ch = 0;
-         zn = 1;
-         Fraction res = new Fraction (ch ,zn);
-         return res;
-         }
-     }
 // метод умножения
-Fraction multiplication(Fraction objF)
-{
-if(objF!=null)
-    {
-         if(this.flagObj==true)
-    {
-        this.reduse();
+
+    Fraction multiplication(Fraction objF) {
+        if (objF != null) {
+            if (this.flagObj == true) {
+                this.reduse();
+            }
+            if (objF.flagObj == true) {
+                objF.reduse();
+            }
+            Fraction res = new Fraction(ch * objF.ch, zn * objF.zn);
+            if (flagCl == true) {
+                res.reduse();
+
+            }
+            return res;
+        } else {
+            ch = 0;
+            zn = 1;
+            Fraction res = new Fraction(ch, zn);
+            return res;
+        }
     }
-     if(objF.flagObj==true)
-     {
-         objF.reduse();
-     }
-     Fraction res = new Fraction (ch * objF.ch,zn * objF.zn);
-    if(flagCl==true)
-         {
-         res.reduse();
-         
-         }
-         return res;
-     }
-else
-    {
-     ch = 0;
-     zn = 1;
-     Fraction res = new Fraction (ch ,zn);
-     return res;
-    }
-}
 // метод сокрашения дроби
-public  void reduse()
-{
-     int nod = Nod(ch,zn);  
-     ch = ch/nod;
-     zn=zn/nod;
-}
+
+    public void reduse() {
+        int nod = Nod(ch, zn);
+        ch = ch / nod;
+        zn = zn / nod;
+    }
 // метод нахождения наибольшего общего делителя
-public  static  int Nod(int chA,int znA)
-  {
+
+    public static int Nod(int chA, int znA) {
         int nod = 1;
         int a = Math.abs(chA);
         int b = Math.abs(znA);
-     if(chA==0||znA==0)
-          return 1;
-     
-         while (a != 0 && b != 0) 
-             {
-             if (a > b )
-                 a = a % b;
-             else
-                 b = b % a;
-             
-             }
-         
-         nod = a + b;
+        if (chA == 0 || znA == 0) {
+            return 1;
+        }
+
+        while (a != 0 && b != 0) {
+            if (a > b) {
+                a = a % b;
+            } else {
+                b = b % a;
+            }
+
+        }
+
+        nod = a + b;
         // System.out.println("Nod = " +nod);
-     return nod;
-  }
+        return nod;
+    }
 }
